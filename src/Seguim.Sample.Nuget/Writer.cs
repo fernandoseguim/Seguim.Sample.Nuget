07c266ba198ccb3450995f10fb68780029382e47
Fernando Seguim <fernando.seguim@gmail.com>
2018-05-27 15:44:07 -0300

    initial commit

---
+++ src/Seguim.Sample.Nuget/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Seguim.Sample.Nuget {
+    public class Writer {
+        public Writer (string author) {
+            this.Author = author ?? throw new ArgumentNullException(nameof(author));
+        }
+        public string Author { get; }
+
+        public void Write (string message) {
+            Console.WriteLine($"Message: {message}");
+            Console.WriteLine();
+            Console.WriteLine($"Author: {Author}");
+        }
+    }
+}
